8ffc90580c14942ff267bed22ebdb5351f3f8b0f
yegeto <115154804+yegeto@users.noreply.github.com>
2024-02-15 00:15:48 +0300

    add grid scale option and different color modes

--- script.js
+++ script.js
@@ -1,25 +1,91 @@
 
-function createBoxes(amount) {
+const createBoxes = amount => {
   const sketchScreen = document.querySelector('#sketchScreen');
   const boxWidth = sketchScreen.offsetWidth / amount;
   for (let index = 0; index < amount**2 ; index++) {
     const box = document.createElement('div');
     box.setAttribute('class', 'box');
-    box.setAttribute('style', `width:${boxWidth}px;height:${boxWidth}px`);
+    box.setAttribute('style', `width:${boxWidth}px;height:${boxWidth}px;background-color:rgb(255, 255, 255);filter:brightness(99.9%)`);
     sketchScreen.appendChild(box);
   }
 }
 
-function paintBlack(nodeList) {
-  nodeList.forEach(box => {
+const paintBlack = () => {
+  const boxesNodeList = document.querySelectorAll('.box');
+  boxesNodeList.forEach(box => {
     box.addEventListener('mouseenter', () => {
+      box.style.filter = 'brightness(99%)';
       box.style.backgroundColor = 'rgb(0,0,0)';
     });
   });
 }
 
-createBoxes(16);
+const paintRainbow = () => {
+  const boxesNodeList = document.querySelectorAll('.box');
+  boxesNodeList.forEach(box => {
+    box.addEventListener('mouseenter', () => {
+      box.style.filter = 'brightness(99%)';
+      const randomRgbValue = () => Math.floor(Math.random() * 256);
+      box.style.backgroundColor = `rgb(${randomRgbValue()}, ${randomRgbValue()}, ${randomRgbValue()})`;
+    });
+  });
+}
 
-const boxesNodeList = document.querySelectorAll('.box');
+function paintDarken() {
+  const boxesNodeList = document.querySelectorAll('.box');
+  boxesNodeList.forEach(box => {
+    box.addEventListener('mouseenter', () => {
+      box.style.backgroundColor = 'rgb(255, 255, 255)';
+      const currentBrightness = box.style.filter.slice(11, 13);
+      box.style.filter = `brightness(${currentBrightness - 10}%)`;
+    });
+  });
+}
 
-paintBlack(boxesNodeList);
+const clearScreen = () => {
+  const sketchScreen = document.querySelector('#sketchScreen');
+  while (sketchScreen.firstChild) {
+    sketchScreen.removeChild(sketchScreen.firstChild);
+  }
+}
+
+const gridSizeBtn = document.querySelector('#gridSizeBtn');
+gridSizeBtn.addEventListener('click', () => {
+  do {
+    gridSize = prompt('Please insert grid size under 10s0');
+  } while (gridSize > 99 || gridSize < 1);
+  clearScreen();
+  createBoxes(gridSize);
+  currentMode();
+});
+
+const paintBlackBtn = document.querySelector('#blackBtn');
+paintBlackBtn.addEventListener('click', () => {
+  currentMode = paintBlack;
+  currentMode();
+});
+
+const paintRainbowBtn = document.querySelector('#rainbowBtn');
+paintRainbowBtn.addEventListener('click', () => {
+  currentMode = paintRainbow;
+  currentMode();
+});
+
+const paintDarkenBtn = document.querySelector('#darkenBtn');
+paintDarkenBtn.addEventListener('click', () => {
+  currentMode = paintDarken;
+  currentMode();
+});
+
+const clearBtn = document.querySelector('#clearBtn');
+clearBtn.addEventListener('click', () => {
+  clearScreen();
+  createBoxes(gridSize);
+  currentMode();
+});
+
+let gridSize = 6;
+let currentMode = paintBlack;
+
+document.addEventListener('DOMContentLoaded', createBoxes(gridSize));
+document.addEventListener('DOMContentLoaded', currentMode);
