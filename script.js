dff471b5f454b42db79e135d39f21143fdf3e913
yegeto <115154804+yegeto@users.noreply.github.com>
2024-02-14 16:13:01 +0300

    a function to create hover effect

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const sketchScreen = document.querySelector('#sketchScreen');
-
 
 function createBoxes(amount) {
+  const sketchScreen = document.querySelector('#sketchScreen');
   const boxWidth = sketchScreen.offsetWidth / amount;
   for (let index = 0; index < amount**2 ; index++) {
     const box = document.createElement('div');
@@ -11,10 +10,16 @@ function createBoxes(amount) {
   }
 }
 
-
-
-
-
-
+function paintBlack(nodeList) {
+  nodeList.forEach(box => {
+    box.addEventListener('mouseenter', () => {
+      box.style.backgroundColor = 'rgb(0,0,0)';
+    });
+  });
+}
 
 createBoxes(16);
+
+const boxesNodeList = document.querySelectorAll('.box');
+
+paintBlack(boxesNodeList);
